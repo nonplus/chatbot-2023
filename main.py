9838b6fd4557b8b096eee009204412661b178554
Stepan Riha <github@nonplus.net>
2023-01-31 00:02:10 +0000

    Implement initial menu #cs-4

--- main.py
+++ main.py
@@ -12,5 +12,37 @@ def get_user_name():
   print("Welcome " + username)
 
 
+def search_restaurant_by_name():
+  print("TODO: Search restaurant by name")
+
+
+def search_restaurant_by_type():
+  print("TODO: Search restaurant by type")
+
+
+def user_preferences():
+  print("TODO: User preferences")
+
+
+def main_menu():
+  while True:
+    print("[1] Search restaurant by name")
+    print("[2] Select restaurant by food type")
+    print("[3] User preferences")
+    print("[Q] Quit")
+    user_input = input("Select an option: ").upper()
+
+    if user_input == '1':
+      search_restaurant_by_name()
+    if user_input == '2':
+      search_restaurant_by_type()
+    if user_input == '3':
+      user_preferences()
+    if user_input == 'Q':
+      break
+    print("")
+
+
 print_welcome()
 get_user_name()
+main_menu()
